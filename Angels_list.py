8624f89e44e609e0e08e3ea6fdc7bb1a85292963
Scott Standring <scott@standring.com>
2017-03-31 16:46:17 -0700

    Stream_Lining code
    change to pandas module, see if the lists can be joined

--- Angels_list.py
+++ Angels_list.py
@@ -4,100 +4,51 @@ import bs4 as bs
 import urllib.request
 import csv
 import lxml
-
-#def San_Fran():
-SF_sauce = urllib.request.urlopen('http://www.legacy.com/ns/obitfinder/obituary-search.aspx?Page=1&countryid=1&daterange=Last24Hrs&stateid=7&keyword=san%20francisco&entriesperpage=50').read()
-SF_soup = bs.BeautifulSoup(SF_sauce, 'lxml')
-SF_data = SF_soup.find_all('div', class_='obitName')
-angel_list=[]
-
-for div in SF_data:
-    name = (div.text)
-    angel=[name]
-    angel_list.append(angel)
-
-with open('SF_angles.csv', 'w') as file:
-    csv_writer = csv.writer(file)
-    for row in angel_list:
-        csv_writer.writerow(row)
-
-OAK_sauce = urllib.request.urlopen('http://www.legacy.com/ns/obitfinder/obituary-search.aspx?Page=1&countryid=1&daterange=Last3Days&stateid=7&keyword=Oakland&entriesperpage=50').read()
-OAK_soup = bs.BeautifulSoup(OAK_sauce, 'lxml')
-OAK_data = OAK_soup.find_all('div', class_='obitName')
-angel_list=[]
-
-for div in OAK_data:
-    name = (div.text)
-    angel=[name]
-    angel_list.append(angel)
-
-with open('OAK_angles.csv', 'w') as file:
-    csv_writer = csv.writer(file)
-    for row in angel_list:
-        csv_writer.writerow(row)
-
-SJ_sauce = urllib.request.urlopen('http://www.legacy.com/ns/obitfinder/obituary-search.aspx?Page=1&countryid=1&daterange=Last3Days&stateid=7&keyword=San%20Jose&entriesperpage=50')
-SJ_soup = bs.BeautifulSoup(SJ_sauce, 'lxml')
-SJ_data = SJ_soup.find_all('div', class_='obitName')
-angel_list=[]
-
-for div in SJ_data:
-    name = (div.text)
-    angel=[name]
-    angel_list.append(angel)
-
-with open('SJ_angels.csv', 'w') as file:
-    csv_writer = csv.writer(file)
-    for row in angel_list:
-        csv_writer.writerow(row)
-
-
-
-
-
-#python3
-#List of Angels
-import bs4 as bs
-import urllib.request
-import csv
-import lxml
 import pandas as pd
 
 
-def San_Fran(SF_data):
-    angel_list=[]
-    for div in SF_data:
+def san_fran(sf_data):
+    sf_list = []
+    for div in sf_data:
         name = (div.text)
         angel=[name]
-        angel_list.append(angel)
-    return angel_list
+        sf_list.append(angel)
+    return sf_list
 
 
-def Oak(Oak_data):
-    angel_list = []
-    for div in OAK_data:
+def oak(oak_data):
+    oak_list = []
+    for div in oak_data:
         name = (div.text)
         angel=[name]
-        angel_list.append(angel)
-    return angel_list
+        oak_list.append(angel)
+    return oak_list
 
-def write_csv(angel_list):
-    raw_data = angel_list
+
+def write_sf_csv(sf_list):
+    raw_data = sf_list
     df = pd.DataFrame(raw_data)
-    df.to_csv('Angels.csv')
-#        with open('Angles.csv', 'w') as file:
-#            csv_writer = csv.writer(file)
-#            for row in angel_list:
-#                csv_writer.writerow(row)
+    df.to_csv('sf_angels.csv')
+
+
+def write_oak_csv(oak_list):
+    raw_data = oak_list
+    df = pd.DataFrame(raw_data)
+    df.to_csv('oak_angels.csv')
 
 
 def main():
-    SF_sauce = urllib.request.urlopen('http://www.legacy.com/ns/obitfinder/obituary-search.aspx?Page=1&countryid=1&daterange=Last24Hrs&stateid=7&keyword=san%20francisco&entriesperpage=50').read()
-    SF_soup = bs.BeautifulSoup(SF_sauce, 'lxml')
-    SF_data = SF_soup.find_all('div', class_='obitName')
-    OAK_sauce = urllib.request.urlopen('http://www.legacy.com/ns/obitfinder/obituary-search.aspx?Page=1&countryid=1&daterange=Last3Days&stateid=7&keyword=Oakland&entriesperpage=50').read()
-    OAK_soup = bs.BeautifulSoup(OAK_sauce, 'lxml')
-    OAK_data = OAK_soup.find_all('div', class_='obitName')
-    SF_list = San_Fran(SF_data)
-    Oak_list = Oak(OAK_data)
-    write_csv(angel_list)
+    sf_sauce = urllib.request.urlopen('http://www.legacy.com/ns/obitfinder/obituary-search.aspx?Page=1&countryid=1&daterange=Last3Days&stateid=7&keyword=san%20francisco&entriesperpage=50').read()
+    oak_sauce = urllib.request.urlopen('http://www.legacy.com/ns/obitfinder/obituary-search.aspx?Page=1&countryid=1&daterange=Last3Days&stateid=7&keyword=Oakland&entriesperpage=50').read()
+    sf_soup = bs.BeautifulSoup(sf_sauce, 'lxml')
+    oak_soup = bs.BeautifulSoup(oak_sauce, 'lxml')
+    sf_data = sf_soup.find_all('div', class_='obitName')
+    oak_data = oak_soup.find_all('div', class_='obitName')
+    sf_list = san_fran(sf_data)
+    oak_list = oak(oak_data)
+    write_sf_csv(sf_list)
+    write_oak_csv(oak_list)
+
+
+if __name__ == '__main__':
+    main()
